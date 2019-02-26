ddcd462eec517446c4e253bd09c723bce685ceeb
notedit <notedit@gmail.com>
2019-02-27 01:02:19 +0800

    fix rmtp out

--- streamer/webrtcstreamer.go
+++ streamer/webrtcstreamer.go
@@ -5,16 +5,19 @@ import (
 	mediaserver "github.com/notedit/media-server-go"
 )
 
+// flvmux  streamable=true
+
+const rtp2rtmp = "appsrc is-live=true do-timestamp=true name=audiosrc ! decodebin ! audioconvert ! audioresample ! audio/x-raw,rate=441000 ! faac ! mux.  videotestsrc num-buffers=500 ! video/x-raw,framerate=25/1 ! x264enc ! mux.  flvmux name=mux ! filesink location=test.flv"
+
 type WebRTCStreamer struct {
 	rtmpUrl string
 
 	audioTrack *mediaserver.IncomingStreamTrack
 	videoTrack *mediaserver.IncomingStreamTrack
 
-	audioPipeline *gstreamer.Pipeline
-	videoPipeline *gstreamer.Pipeline
-	audiosrc      *gstreamer.Element
-	videosrc      *gstreamer.Element
+	pipeline *gstreamer.Pipeline
+	audiosrc *gstreamer.Element
+	videosrc *gstreamer.Element
 }
 
 func NewWebRTCStreamer(rtmpUrl string, audioTrack *mediaserver.IncomingStreamTrack, videoTrack *mediaserver.IncomingStreamTrack) *WebRTCStreamer {
@@ -22,5 +25,33 @@ func NewWebRTCStreamer(rtmpUrl string, audioTrack *mediaserver.IncomingStreamTra
 	streamer := &WebRTCStreamer{}
 	streamer.rtmpUrl = rtmpUrl
 
-	return nil
+	streamer.audioTrack = audioTrack
+	streamer.videoTrack = videoTrack
+
+	return streamer
+}
+
+func (self *WebRTCStreamer) setupPipeline() {
+
+	pipeline, err := gstreamer.New(rtp2rtmp)
+	if err != nil {
+		panic(err)
+	}
+
+	self.pipeline = pipeline
+	self.audiosrc = pipeline.FindElement("audiosrc")
+	self.pipeline.Start()
+}
+
+func (self *WebRTCStreamer) PushAudioFrame(data []byte, timestamp uint) {
+
+	if self.pipeline == nil {
+		self.setupPipeline()
+	}
+
+	self.audiosrc.Push(data)
+}
+
+func (self *WebRTCStreamer) PushVideoFrame(data []byte, timestamp uint) {
+	// todo
 }
